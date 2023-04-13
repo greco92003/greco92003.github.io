f29e060b50824a916276bd302bec251d9e82f2f5
Greco Marques <greco92003@gmail.com>
2023-04-13 14:50:15 -0300

    parallax site created

---
+++ script.js
@@ -0,0 +1,17 @@
+let text = document.getElementById('text');
+let foguete = document.getElementById('foguete');
+let nuvemesq1 = document.getElementById('nuvemesq1');
+let nuvemesq2 = document.getElementById('nuvemesq2');
+let nuvemdir1 = document.getElementById('nuvemdir1');
+let nuvemdir2 = document.getElementById('nuvemdir2');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    foguete.style.top = value * -1.5 + 'px';
+    nuvemdir1.style.left = value * 1.5 + 'px';
+    nuvemdir2.style.left = value * 0.8 + 'px';
+    nuvemesq1.style.left = value * -1.5 + 'px';
+    nuvemesq2.style.left = value * -0.8 + 'px';
+})
