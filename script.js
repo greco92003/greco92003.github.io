878066e9575c332c4dd23deafb28aaef3f100710
Greco Marques <greco92003@gmail.com>
2023-04-13 17:19:31 -0300

    add footer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let text = document.getElementById('text');
 let foguete = document.getElementById('foguete');
+let fogo = document.getElementById('fogo');
 let nuvemesq1 = document.getElementById('nuvemesq1');
 let nuvemesq2 = document.getElementById('nuvemesq2');
 let nuvemdir1 = document.getElementById('nuvemdir1');
@@ -10,6 +11,7 @@ window.addEventListener('scroll', () => {
 
     text.style.marginTop = value * 2.5 + 'px';
     foguete.style.top = value * -1.5 + 'px';
+    fogo.style.top = value * -1.5 + 'px';
     nuvemdir1.style.left = value * 1.5 + 'px';
     nuvemdir2.style.left = value * 0.8 + 'px';
     nuvemesq1.style.left = value * -1.5 + 'px';
